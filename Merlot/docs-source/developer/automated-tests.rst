289589d664d1efd6a196f335ba235cae5f72242d
Silvestre Huens <s.huens@gmail.com>
2011-01-21 22:36:13 -0300

    starting the addition of the graph funcionality in the task listing

---
+++ Merlot/docs-source/developer/automated-tests.rst
@@ -0,0 +1,13 @@
+Automated tests in doctest style
+================================
+
+The application is covered by tests that are written using the `doctest` style.
+In this section you will find all those tests.
+
+.. include:: _doctests/setup.rst
+.. include:: _doctests/starred_tasks.rst
+.. include:: _doctests/catalog.rst
+.. include:: _doctests/functional_auth.rst
+.. include:: _doctests/functional_simpleproject.rst
+.. include:: _doctests/functional_reports.rst
+
---
+++ Merlot/merlot/vocabularies.py
@@ -0,0 +1,52 @@
+"""Vocabularies"""
+
+import grok
+
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory, ISource
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from z3c.objpath.interfaces import IObjectPath
+from zope.app.authentication.interfaces import IAuthenticatorPlugin
+
+from merlot import interfaces as ifaces
+
+
+class ProjectsVocabulary(grok.GlobalUtility):
+    """A vocabulary with all the existing projects"""
+    grok.implements(ISource, IVocabularyFactory)
+    grok.provides(IVocabularyFactory)
+    grok.name('merlot.ProjectsVocabulary')
+
+    def __call__(self, context=None):
+        site = grok.getSite()
+        path = getUtility(IObjectPath).path
+
+        projects = [i for i in site['projects'].values() if \
+                    ifaces.IProject.providedBy(i)]
+        title = lambda p: '%s (%s)' % (p.title, p.client.to_object.title)
+
+        projects = [SimpleTerm(path(p), path(p), title(p)) for p in projects]
+
+        all_projects = ('all', 'all', 'All projects')
+        projects.insert(0, SimpleTerm(*all_projects))
+
+        return SimpleVocabulary(projects)
+
+
+class UsersVocabulary(grok.GlobalUtility):
+    """A vocabulary with all the existing users"""
+    grok.implements(ISource, IVocabularyFactory)
+    grok.provides(IVocabularyFactory)
+    grok.name('merlot.UsersVocabulary')
+
+    def __call__(self, context=None):
+        site = grok.getSite()
+        users = site['users'].values()
+        users = [SimpleTerm(u.username, u.username, u.real_name) for \
+                 u in users]
+
+        all_users = ('all', 'all', 'All users')
+        users.insert(0, SimpleTerm(*all_users))
+
+        return SimpleVocabulary(users)
