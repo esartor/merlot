8f6cdef8f1fab2341909d0ddd2d1f1d070824efe
Silvestre Huens <s.huens@gmail.com>
2011-02-01 05:27:54 -0300

    worked hours, exceded hours and remaing hours chart added

--- Merlot/merlot/static/merlot.js
+++ Merlot/merlot/static/merlot.js
@@ -286,85 +286,119 @@ function listingGraphs() {
     var today_date = $('.today-date').html();
     var table = $('#listing-table');
     var table_rows = $('tr', table);
-    var th = $('<th class="days-status-header">To end</th>');
+    var th = $('<th class="days-status-header">Deadline</th>');
     var start_date_header = $('.start-date-header', table);
     var end_date_header = $('.end-date-header', table);
     var estimate_header = $('.estimate-header', table);
+    var remaining_header = $('.remaining-header', table);
     var hours_header = $('.hours-header', table);    
         
-    var hoursStatus = function(row) {
+    var hoursGraph = function(row) {
         //layout setup
-        var canvas_elem = '<canvas width="200" height="30"></canvas>'
-        var est_dom = $('.estimate', row);
-        var woh_dom = $('.worked-hours', row);
-        est_dom.before('<td class="hours-graph">'+canvas_elem+'</td>');
+        var canvas_elem = '<canvas width="230" height="30"></canvas>'
+        var est_el = $('.estimate', row);
+        var woh_el = $('.worked-hours', row);
+        var rem_el = $('.remaining', row);
+        est_el.before('<td class="hours-graph">'+canvas_elem+'</td>');
         
         var th_hoursusage = $('<th class="hours-usage-header">Hours Usage</th>');
         estimate_header.before(th_hoursusage);
         estimate_header.remove();
+        remaining_header.remove();
         hours_header.remove();
         
-        var estimation = est_dom.html() ? est_dom.html()*1 : 0;
-        var worked_hours = woh_dom.html() ? woh_dom.html()*1 : 0;
+        var estimation = est_el.html() ? est_el.html()*1 : 0;
+        var worked_hours = woh_el.html() ? woh_el.html()*1 : 0;
+        var remaining_hours = rem_el.html() ? rem_el.html()*1 : 0;
         if (estimation) {
+            //bounding box(worked_hours / estimation) * 100;
+            var box = 230;
+            var lmargin = 15;
+            var rmargin = 15; 
+            var tmargin = 30;
+            var bmargin = 30;
+            var bb_start = lmargin; 
+            var bb_end = box - rmargin;
+            
             //set scale
-            max_n = estimation > worked_hours ? estimation : worked_hours
             canvas = $('.hours-graph canvas', row)[0];
             var ctx = canvas.getContext('2d');
             
             var porcentage = (worked_hours / estimation) * 100;
             var proportion = porcentage * 2 
+
+            if (remaining_hours) {
+                var remaing = (remaining_hours / estimation) * 100;
+            } else {
+                var remaing = 0;
+            }
             
-            var max_porcentage = 100;
+            var porcentage_100 = 100;
             var exceded = 0;
-            if (proportion > 200) {
-                max_porcentage = Math.round(porcentage);
-                exceded = proportion - 200;
+            if (porcentage > 100) {
+                porcentage_100 = Math.round(porcentage + remaing);
                 //we have to calculate the new proportion and exceded value
-                proportion = Math.round((200 / proportion) * 200);
-                exceded = Math.round((exceded / proportion) * 200);
+                proportion = Math.round((200 / (proportion + remaing*2)) * 200);
+                exceded = Math.round(200 -(proportion + remaing*2)); 
+                remaing = remaing*2               
+            } else {
+                // we have to check if the remaining hours are exceding the total
+                if (porcentage + remaing > 100) {
+                    porcentage_100 = Math.round(porcentage + remaing);
+                    proportion = (((worked_hours + remaining_hours) / estimation) * 100)*2;
+                    proportion = Math.round((200 / (proportion)) * 200);
+                    remaing = Math.round(200 -(proportion));
+                } else {
+                    remaing = remaing*2
+                }
             }
             
             var box_height = 15;
             //border box
             ctx.fillStyle = "#CCC";
-            ctx.strokeRect(0, 0, 200, box_height);
+            ctx.strokeRect((0 + bb_start), 0, 200, box_height);
             
             //background box
             ctx.fillStyle = "#EEE";
-            ctx.fillRect(1, 1, 200, box_height);          
+            ctx.fillRect(1 + bb_start, 1, 200, box_height);          
 
             //used time
             ctx.fillStyle = "#6AB42D";
-            ctx.fillRect(1, 1, proportion, box_height);      
+            ctx.fillRect(1 + bb_start, 1, proportion, box_height);      
            
             //exceded time
             if (exceded) {
                 ctx.fillStyle = "#D61313";
-                ctx.fillRect((1+proportion), 1, exceded, box_height);
+                ctx.fillRect((1+proportion + bb_start), 1, exceded, box_height);
+            }
+            
+            if (remaing) {
+                ctx.fillStyle = "#BED4EB";
+                ctx.fillRect((1+(exceded ? exceded+proportion : proportion )+ bb_start), 1, remaing, box_height);
             }
             
             //text 0%
             ctx.fillStyle = "#000";            
             ctx.textBaseline = "top";
             ctx.fillText("0%", 0, 20);
+            //text 100 %
+            ctx.textAlign = "center";
+            ctx.fillText(porcentage_100 + "%", 200, 20);
 
             //text not exceded %
             if (porcentage > 0 ) {
                 ctx.textAlign = "left";
-                ctx.fillText((porcentage < 100 ? Math.round(porcentage) : '100') + "%", proportion, 5);
+                ctx.fillText((porcentage < 100 ? Math.round(porcentage) : '100') + "%", proportion+3, 5);
             }
-            //text max %
-            ctx.textAlign = "end";
-            ctx.fillText(max_porcentage + "%", 200, 20);
         }
         
         //lets remove the unused columns
-        woh_dom.remove();
-        est_dom.remove();
+        woh_el.remove();
+        rem_el.remove();
+        est_el.remove();
     };
     table_rows.each(function(){
-        hoursStatus($(this));
+        hoursGraph($(this));
         var td = $('<td class="days-remaining"></td>');
         var start_date_dom  = $('.start-date', $(this));
         var end_date_dom = $('.end-date', $(this));               
@@ -375,7 +409,16 @@ function listingGraphs() {
             var e = Date.parse(end_date);
             var diff = e-s;
             var days = Math.floor(diff / (1000*60*60*24));
-            td.html(days +' days');
+            if (days < 0) {
+                var days_status = ' past';
+            } else {
+                var days_status = ' before';
+            }
+            td.html(days +' days' + days_status);
+        } else {
+            if (!end_date) {
+                td.html('no deadline');
+            }
         }
         start_date_dom.before(td);
         start_date_dom.remove();
